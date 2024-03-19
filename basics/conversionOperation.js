eba07c4059dd7583aad0d49fd7d0df8d11e87839
prathameshkakade <143092328+prathameshkakade@users.noreply.github.com>
2024-03-19 09:05:39 +0000

    conversion of data types

---
+++ basics/conversionOperation.js
@@ -0,0 +1,27 @@
+let score=true
+
+
+console.log(typeof score);
+let valueInNumber =Number (score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+//NaN not a number
+//
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+
+//1 =>true
+//" "=>false
+//"pratham" => ture
+
+
+let someNumber = 33
+let stringNumber=String(someNumber)
+console.log(typeof someNumber);
+console.log( someNumber);
---
+++ basics/variable.js
@@ -0,0 +1,19 @@
+const accountId =1234354
+let accountEmail ="pratha2gmail.com"
+var accountPassword="1234567"
+accountCity="jaipur"
+//not assign any value to object that print undefined
+ let accountState;
+
+//accountId=2
+
+accountEmail="hdbd@gmail.com"
+accountPassword="1321654"
+accountCity ="pune"
+
+/*  prefer not use var 
+ because issue of block scope and functional scope*/
+
+
+console.log(accountId);
+console.table([accountId,accountEmail ,accountPassword ,accountCity,accountState])
